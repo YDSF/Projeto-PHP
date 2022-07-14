5bc8aeac777dbcf02062c3889e56e6ea982e81d0
yasmim <yasmim.25022003@gmail.com>
2022-07-14 16:57:24 -0300

    atual

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>Olá Mundo</h1>";
+    ?>
+</body>
+
+</html>
---
+++ model/usuario.php
@@ -0,0 +1,102 @@
+<?php
+
+class Usuario
+{
+    private $IdUsuario;
+    private $NomeUsuario;
+    private $Usuario;
+    private $Senha;
+
+
+
+    /**
+     * Get the value of IdUsuario
+     */
+    public function getIdUsuario()
+    {
+        return $this->IdUsuario;
+    }
+
+    /**
+     * Set the value of IdUsuario
+     *
+     * @return  self
+     */
+    public function setIdUsuario($IdUsuario)
+    {
+        $this->IdUsuario = $IdUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of NomeUsuario
+     */
+    public function getNomeUsuario()
+    {
+        return $this->NomeUsuario;
+    }
+
+    /**
+     * Set the value of NomeUsuario
+     *
+     * @return  self
+     */
+    public function setNomeUsuario($NomeUsuario)
+    {
+        $this->NomeUsuario = $NomeUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Usuario
+     */
+    public function getUsuario()
+    {
+        return $this->Usuario;
+    }
+
+    /**
+     * Set the value of Usuario
+     *
+     * @return  self
+     */
+    public function setUsuario($Usuario)
+    {
+        $this->Usuario = $Usuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Senha
+     */
+    public function getSenha()
+    {
+        return $this->Senha;
+    }
+
+    /**
+     * Set the value of Senha
+     *
+     * @return  self
+     */
+    public function setSenha($Senha)
+    {
+        $this->Senha = $Senha;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        $User = "Usuário:<br/>"
+            . " - Nome Usuário: " . $this->getNomeUsuario() . "<br/>"
+            . " - Usuário: " . $this->getUsuario() . "<br/>";
+
+        return $User;
+    }
+
+    
+}
