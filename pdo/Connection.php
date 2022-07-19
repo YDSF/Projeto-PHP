ccd95610749d78086f9e3a53c6493861f7f2680b
yasmim <yasmim.25022003@gmail.com>
2022-07-19 14:48:34 -0300

    continuação

--- pdo/Connection.php
+++ pdo/Connection.php
@@ -3,21 +3,20 @@
 use FTP\Connection as FTPConnection;
 
 require_once 'Config.php';
-class Connection{
+class Connection {
     public static function getConnection($host,$dbName, $userName, $pass){
         $dns = "mysql:host=$host;dbname=$dbName;charset=UTF8";
 
         try{
-            $option = (PDO::ATTR_ERRMODE -> PDO::ATTR_ERRMODE_EXCEPTION);
+            $option = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
             return new PDO($dns,$userName,$pass,$option);
-        } 
-    } catch (PDOExrecption $ex){
+        } catch (PDOException $ex) {
         die ($ex ->getMessage());
     }
+}
 
 }
 
-return Connection::getConnection($host, $dbName, $userName, $pass)
-
+return Connection::getConnection($host, $dbName, $userName, $pass);
 
 ?>
